8d060dd6c6b7a8fea7f42e26433c6c1a6bed6d17
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2020-02-20 06:35:05 -0700

    feat: add recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -71,3 +71,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title="Example Title",
+            time_minutes=5,
+            price=5.99
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
